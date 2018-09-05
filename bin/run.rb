587960a6ccc1edff82f1367491e870d312e3f9d9
kruchkamgar <kruchkamgar@gmail.com>
2018-09-05 18:39:52 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,11 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+pokemon_array = @db.execute("SELECT *
+  FROM pokemon
+  WHERE pokemon.id = ?", 1).flatten
+
+this_pokemon = Pokemon.new(id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], db: @db)
+
+binding.pry
+0
