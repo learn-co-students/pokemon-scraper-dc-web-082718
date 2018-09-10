b08da419df5ac2e01dee8a2b9649f13f39f93995
github username <github email address>
2018-09-10 15:44:07 -0400

    Set up sqlite db and used ruby to query sql.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(hash)
+    @id = hash[:id]
+    @name = hash[:name]
+    @type = hash[:type]
+    @db = hash[:db]
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    pokemon_data = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    hash = Hash.new
+    hash[:id] = pokemon_data[0]
+    hash[:name] = pokemon_data[1]
+    hash[:type] = pokemon_data[2]
+    hash[:db] = pokemon_data[3]
+    Pokemon.new(hash)
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+  end
+
 end
